3d7b7439332a4abfe777b03846b26cadd55f0522
reddberckley <danielonodje@gmail.com>
2016-08-22 11:47:05 +0100

    calls api

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+var request = require('superagent');
+var co = require('co');
+var chalk = require('chalk');
+// console.log("Hello, World!");
+
+request
+    .get("https://got-quotes.herokuapp.com/quotes")
+    .end(function(err,res){
+        // console.log(res.body);
+        console.log(chalk.green(res.body.quote));
+        console.log(chalk.red("   - "+res.body.character));
+    });
